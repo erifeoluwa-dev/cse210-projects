59f801895635d1bda90c974dbdca71d629c82f51
erifeoluwa-dev <olajmklove08064@email.com>
2026-09-11 10:25:00 +0100

    resume build up

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "WebDesign";
+        job1._startYear = 2025;
+        job1._stopYear = 2027;
+        
+        Job job2 = new Job();
+        job2._company = "Total Energy";
+        job2._jobTitle = "Data Analysis";
+        job2._startYear = 2026;
+        job2._stopYear = 2027;
+        
+        Resume myResume = new Resume();
+        myResume._name = "Ojelade Ifeoluwa";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
 }
